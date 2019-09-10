2cf7baf2e6e194736596d9c2bc589703f0113bcf
kvazar2569 <49428870+kvazar2569@users.noreply.github.com>
2019-09-10 10:15:13 +0300

    q

---
+++ 2лаб/2лаб/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2лаб
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 1.0;
+
+            for (int k = 0; k < 200; k++)
+            {
+                sum *= (Math.Pow(-1, k) * k + 8) / (2 * Math.Pow(k, 3) + k + 7);
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
