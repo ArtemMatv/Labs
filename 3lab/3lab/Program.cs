46fa682cd9043b113156771bb730ab91f12013aa
kvazar2569 <49428870+kvazar2569@users.noreply.github.com>
2019-09-16 20:44:38 +0300

    q

---
+++ 3lab/3lab/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3lab
+{
+    class Program
+    {
+        static private int[] Partitioning(int number)
+        {
+            int[] result = new int[3];
+
+            result[2] = number % 10;
+            result[1] = (number % 100 - result[2]) / 10;
+            result[0] = (number - result[1] * 10 - result[0]) / 100;
+                
+            return result;
+        }
+
+        static void Main()
+        {
+            int number;
+            do {
+                Console.WriteLine("Enter three digit number: ");
+
+                int.TryParse(Console.ReadLine(), out number);
+
+                if (number > 999 || number < 100)
+                    Console.WriteLine("You've entered wrong value!");
+
+            } while (number > 999 || number < 100);
+
+            int[] comparing = Partitioning(number);
+
+            if (((comparing[0] > comparing[1]) && (comparing[1] > comparing[2]))||((comparing[0] < comparing[1]) && (comparing[1] < comparing[2])))
+                Console.WriteLine("Makes");
+            else
+                Console.WriteLine("Does not make");
+
+            Console.ReadKey();
+        }
+    }
+}
