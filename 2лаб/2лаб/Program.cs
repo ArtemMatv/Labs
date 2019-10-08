e95b1e068e5bfea3c31233fa70f54d0e50d99277
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2019-10-08 10:56:21 +0300

    Update Program.cs

--- 2лаб/2лаб/Program.cs
+++ 2лаб/2лаб/Program.cs
@@ -13,11 +13,18 @@ namespace _2лаб
             //Знайти добуток (((-1)^k)*k))/(2*(k^3)+k+7)
             
             double sum = 1.0;
-
-            for (int k = 0; k < 200; k++)
-            {
-                sum *= (Math.Pow(-1, k) * k + 8) / (2 * Math.Pow(k, 3) + k + 7);
-            }
+            
+            Console.Write("Enter start number: ");
+            int.TryParse(Console.ReadLine(), out int k);
+            
+            Console.Write("Enter end number: ");
+            int.TryParse(Console.ReadLine(), out int end);
+            
+            if (k >= 0 && k <= end)
+                for (k = 0; k < end; k++)
+                    sum *= (Math.Pow(-1, k) * k + 8) / (2 * Math.Pow(k, 3) + k + 7);
+            else
+                Console.WriteLine("Error!!!!");
 
             Console.WriteLine(sum);
             Console.ReadKey();
