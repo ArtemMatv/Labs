cb031634f7a81bd59179764c6f7b3e65f7f9d21a
kvazar2569 <49428870+kvazar2569@users.noreply.github.com>
2019-10-20 12:20:56 +0300

    8lab changed

--- 8lab/8lab/Car.cs
+++ 8lab/8lab/Car.cs
@@ -4,48 +4,29 @@
     {
         public string Name { get; set; }
 
-        public int Year
-        {
-            get
-            {
-                return Year;
-            }
-            private set
-            {
-                if (value < 1879)
-                    Year = value;
-
-                else if (value > 2019)
-                    Year = 2019;
-
-                else
-                    Year = value;
-            }
-        }
+        public int Year { get; private set; }
 
         public string Color { get; set; }
 
-        public int Speed
-        {
-            get
-            {
-                return Speed;
-            }
-            private set
-            {
-                if (value <= 0)
-                    Speed = 1;
-                else
-                    Speed = value;
-            }
-        }
+        public int Speed { get; private set; }
 
         public Car(string name, int year, string color, int speed)
         {
             Name = name;
-            Year = year;
+
+            if (year < 1879)
+                Year = 1879;
+            else if (year > 2019)
+                Year = 2019;
+            else
+                Year = year;
+
             Color = color;
-            Speed = speed;
+
+            if (speed <= 0)
+                Speed = 1;
+            else
+                Speed = speed;
         }
     }
 }
