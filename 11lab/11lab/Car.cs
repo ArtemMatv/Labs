48a2725bd0c9918767aeab0861672d1b9dbff62c
kvazar2569 <kvazar2569@gmail.com>
2019-11-19 09:55:08 +0200

    11th lab added

---
+++ 11lab/11lab/Car.cs
@@ -0,0 +1,31 @@
+﻿namespace _11lab
+{
+    public class Car
+    { 
+        public Car()
+        {
+            CamberClimbed = false;
+            Painted = false;
+            OilChanged = false;
+            InspectionDone = false;
+            WeelsChanged = false;
+            BodyRepaired = false;
+        }
+        public bool CamberClimbed { get; set; }
+        public bool Painted { get; set; }
+        public bool OilChanged { get; set; }
+        public bool InspectionDone { get; set; }
+        public bool WeelsChanged { get; set; }
+        public bool BodyRepaired { get; set; }
+
+        public override string ToString()
+        {
+            return $"CamberClimbed -- {CamberClimbed}\n" +
+                $"Painted -- {Painted}\n" +
+                $"OilChanged -- {OilChanged}\n" +
+                $"InspectionDone -- {InspectionDone}\n" +
+                $"WeelsChanged -- {WeelsChanged}\n" +
+                $"BodyRepaired -- {BodyRepaired}\n";
+        }
+    }
+}
---
+++ 11lab/11lab/ServiceStation.cs
@@ -0,0 +1,48 @@
+﻿namespace _11lab
+{
+    class ServiceStation
+    {
+        public delegate void Do(Car car);
+
+        public Do DoAll;
+        public Do DoPartial;
+
+        public ServiceStation()
+        {
+            DoAll = ClimbeCamber;
+            DoAll += PaintCar;
+            DoAll += ChangeOil;
+            DoAll += DoInspection;
+            DoAll += ChangeWeels;
+            DoAll += RepaireBody;
+
+            DoPartial = PaintCar;
+            DoPartial += ChangeWeels;
+            DoPartial += RepaireBody;
+        }
+        public void ClimbeCamber(Car car)
+        {
+            car.CamberClimbed = true;
+        }
+        public void PaintCar(Car car)
+        {
+            car.Painted = true;
+        }
+        public void ChangeOil(Car car)
+        {
+            car.OilChanged = true;
+        }
+        public void DoInspection(Car car)
+        {
+            car.InspectionDone = true;
+        }
+        public void ChangeWeels(Car car)
+        {
+            car.WeelsChanged = true;
+        }
+        public void RepaireBody(Car car)
+        {
+            car.BodyRepaired = true;
+        }
+    }
+}
