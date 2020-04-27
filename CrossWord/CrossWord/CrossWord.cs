4261972cdaee3d745a288da243714b1c55bdbd3b
kvazar2569 <kvazar2569@gmail.com>
2020-04-27 15:45:06 +0300

    oh my GOD I did itgit add .git add .git add .git add .git add .

---
+++ CrossWord/CrossWord/CrossWord.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CrossWord
+{
+    public partial class CrossWord : Form
+    {
+        private readonly XmlDocument _configuration;
+        private int _widthCapacity;
+        private int _heightCapacity;
+        public CrossWord()
+        {
+            InitializeComponent();
+
+            _configuration = new XmlDocument();
+            _configuration.Load(ConfigurationManager.AppSettings["configuration"]);
+
+            FindCapacities();
+            BuildCrossword();
+            AddDefinitions();
+        }
+
+        private void FindCapacities()
+        {
+            _widthCapacity = 0;
+            _heightCapacity = 0;
+
+            foreach (XmlElement word in _configuration.DocumentElement.FirstChild)
+            {
+                int x = int.Parse(word.Attributes["x"].Value);
+                int y = int.Parse(word.Attributes["y"].Value);
+                int length = int.Parse(word.Attributes["length"].Value);
+
+                if (x + length > _widthCapacity
+                    && word.Attributes["position"].Value == "0")
+                {
+                    _widthCapacity = x + length;
+                }
+
+                if (y + length > _heightCapacity
+                    && word.Attributes["position"].Value == "1")
+                {
+                    _heightCapacity = y + length;
+                }
+            }
+        }
+
+        private void BuildCrossword()
+        {
+            TableLayoutPanel crossword = new TableLayoutPanel()
+            {
+                ColumnCount = _widthCapacity,
+                RowCount = _heightCapacity,
+                Width = int.Parse(_configuration.DocumentElement.FirstChild.Attributes["width"].Value) + 5,
+                Height = int.Parse(_configuration.DocumentElement.FirstChild.Attributes["height"].Value) + 5,
+                Location = new Point(0, 0),
+                BackColor = Color.Red
+            };
+
+
+            int elementWidth = (int.Parse(_configuration.DocumentElement.FirstChild.Attributes["width"].Value)
+                - _widthCapacity * 5) / _widthCapacity;
+
+            int elementHeight = (int.Parse(_configuration.DocumentElement.FirstChild.Attributes["height"].Value)
+                - _heightCapacity * 5) / _heightCapacity;
+
+            List<string> positions = new List<string>();
+
+
+            foreach (XmlElement conf in _configuration.DocumentElement.FirstChild)
+            {
+                int x = int.Parse(conf.Attributes["x"].Value);
+                int y = int.Parse(conf.Attributes["y"].Value);
+                int length = int.Parse(conf.Attributes["length"].Value);
+
+                if (conf.Attributes["position"].Value == "0")
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        RichTextBox box = new RichTextBox()
+                        {
+                            MaxLength = 1,
+                            Width = elementWidth,
+                            Height = elementHeight,
+                            Font = new Font("Arial", elementWidth / 2)
+                        };
+
+                        if (!positions.Contains((x + i).ToString() + ";" + y))
+                        {
+                            positions.Add((x + i).ToString() + ";" + y);
+                            crossword.Controls.Add(box, x + i, y);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < length; i++)
+                    {
+                        RichTextBox box = new RichTextBox()
+                        {
+                            MaxLength = 1,
+                            Width = elementWidth,
+                            Height = elementHeight,
+                            Font = new Font("Arial", elementWidth / 2)
+                        };
+
+                        if (!positions.Contains(x + ";" + (y + i).ToString()))
+                        {
+                            positions.Add(x + ";" + (y + i).ToString());
+                            crossword.Controls.Add(box, x, y + i);
+                        }
+                    }
+                }
+            }
+            Controls.Add(crossword);
+        }
+        private void AddDefinitions()
+        {
+            int i = 0;
+            foreach (XmlElement item in _configuration.DocumentElement.LastChild)
+            {
+                string position = _configuration.DocumentElement.FirstChild.ChildNodes[i].Attributes["position"].Value == "0"
+                    ? "горизонталь" : "вертикаль";
+                Label definition = new Label()
+                {
+                    Text = (i + 1) + " (" + position + "): " + item.FirstChild.InnerText,
+                    Font = new Font("Arial", 15),
+                    Location = new Point(
+                        int.Parse(_configuration.DocumentElement.FirstChild.Attributes["width"].Value) + 10,
+                        30 * i)
+                };
+
+                definition.Width = Width - definition.Location.X;
+
+                SizeChanged += (s, e) =>
+                {
+                    definition.Width = Width - definition.Location.X;
+                };
+
+                Controls.Add(definition);
+                i++;
+            }
+        }
+    }
+}
---
+++ CrossWord/CrossWord/Form1.cs
@@ -0,0 +1,375 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CrossWord
+{
+    public partial class Form1 : Form
+    {
+        private XmlDocument _configuration;
+        private int _amount;
+        private readonly string _configPath;
+        private List<int> lengths;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Width = 580;
+            Height = 600;
+
+            Button usePrevious = new Button()
+            {
+                Location = new Point(360, 25),
+                Size = new Size(200, 30),
+                Text = "Використати останню конфігурацію"
+            };
+            usePrevious.Click += (s, e) =>
+            {
+                CrossWord crossWord = new CrossWord();
+
+                crossWord.FormClosed += (senser, evArgs) =>
+                {
+                    Close();
+                };
+
+                crossWord.Show();
+                Hide();
+            };
+            Controls.Add(usePrevious);
+
+            Text = "Генератор кросвордів";
+
+            _amount = 0;
+            _configPath = ConfigurationManager.AppSettings["configuration"];
+
+            CreateStartControls();
+        }
+
+        private void CreateStartControls()
+        {
+            Label wLabel = new Label()
+            {
+                Location = new Point(10, 10),
+                Size = new Size(100, 20),
+                Text = "Ширина:"
+            };
+            NumericUpDown width = new NumericUpDown()
+            {
+                Maximum = 1366,
+                Minimum = 100,
+                Width = 100,
+                Location = new Point(10, 30)
+            };
+
+            Label hLabel = new Label()
+            {
+                Location = new Point(120, 10),
+                Size = new Size(100, 20),
+                Text = "Висота:"
+            };
+            NumericUpDown height = new NumericUpDown()
+            {
+                Maximum = 768,
+                Minimum = 100,
+                Width = 100,
+                Location = new Point(120, 30)
+            };
+
+            Button buttonAccept = new Button()
+            {
+                Text = "Підтвердити",
+                Size = new Size(100, 30),
+                Location = new Point(250, 25)
+            };
+            buttonAccept.Click += (s, e) =>
+            {
+                if (_amount > 1)
+                {
+                    SaveConfiguration(width.Value, height.Value);
+                    GoToWordCreating();
+                }
+                else
+                {
+                    Label ex = new Label()
+                    {
+                        Location = new Point(370, 20),
+                        Width = 150,
+                        Text = "Too few words!",
+                        Font = new Font("Arial", 12),
+                        ForeColor = Color.Red
+                    };
+                    Controls.Add(ex);
+                }
+            };
+
+            Label xLabel = new Label()
+            {
+                Location = new Point(10, 60),
+                Size = new Size(40, 20),
+                Text = "X:"
+            };
+            NumericUpDown wordX = new NumericUpDown()
+            {
+                Maximum = 100,
+                Minimum = 0,
+                Width = 40,
+                Location = new Point(10, 80)
+            };
+
+            Label yLabel = new Label()
+            {
+                Location = new Point(60, 60),
+                Size = new Size(40, 20),
+                Text = "Y:"
+            };
+            NumericUpDown wordY = new NumericUpDown()
+            {
+                Maximum = 100,
+                Minimum = 0,
+                Width = 40,
+                Location = new Point(60, 80)
+            };
+
+            Label lengthLabel = new Label()
+            {
+                Location = new Point(110, 60),
+                Size = new Size(60, 20),
+                Text = "Довжина:"
+            };
+            NumericUpDown wordLength = new NumericUpDown()
+            {
+                Maximum = 100,
+                Minimum = 1,
+                Width = 60,
+                Location = new Point(110, 80)
+            };
+
+            Label positionLabel = new Label()
+            {
+                Location = new Point(190, 60),
+                Size = new Size(100, 20),
+                Text = "Положення:"
+            };
+            ComboBox wordPosition = new ComboBox()
+            {
+                Location = new Point(190, 80)
+            };
+
+            wordPosition.Items.Add("Горизонтальне");
+            wordPosition.Items.Add("Ветикальне");
+            wordPosition.SelectedIndex = 0;
+
+            Button buttonAddConfig = new Button()
+            {
+                Location = new Point(330, 79),
+                Text = "Додати слово"
+            };
+            buttonAddConfig.Click += (s, e) =>
+            {
+                AddConfig(wordX, wordY, wordLength, wordPosition);
+            };
+
+            Controls.Add(xLabel);
+            Controls.Add(wordX);
+            Controls.Add(yLabel);
+            Controls.Add(wordY);
+            Controls.Add(lengthLabel);
+            Controls.Add(wordLength);
+            Controls.Add(positionLabel);
+            Controls.Add(wordPosition);
+            Controls.Add(buttonAddConfig);
+
+            Controls.Add(wLabel);
+            Controls.Add(width);
+            Controls.Add(hLabel);
+            Controls.Add(height);
+            Controls.Add(buttonAccept);
+        }
+
+        private void SaveConfiguration(decimal cWidth, decimal cHeight)
+        {
+            XmlNode config = _configuration.DocumentElement.FirstChild;
+
+            XmlAttribute width = _configuration.CreateAttribute("width");
+            XmlAttribute height = _configuration.CreateAttribute("height");
+
+            width.AppendChild(_configuration.CreateTextNode(cWidth.ToString()));
+            height.AppendChild(_configuration.CreateTextNode(cHeight.ToString()));
+
+            config.Attributes.Append(width);
+            config.Attributes.Append(height);
+
+            _configuration.DocumentElement.AppendChild(config);
+        }
+
+        private void GoToWordCreating()
+        {
+            Controls.Clear();
+            Width = 665;
+            Height = 600;
+
+            Label nameLabel = new Label()
+            {
+                Location = new Point(10, 10),
+                Size = new Size(100, 20),
+                Text = "Слово:"
+            };
+
+            Label definitionLabel = new Label()
+            {
+                Location = new Point(120, 10),
+                Size = new Size(100, 20),
+                Text = "Визначення:"
+            };
+
+            for (int i = 0; i < _amount; i++)
+            {
+
+                TextBox wordName = new TextBox()
+                {
+                    Location = new Point(10, 30 + i * 50),
+                    Width = 100,
+                    MaxLength = lengths[i]
+                };
+
+                RichTextBox wordDefinition = new RichTextBox()
+                {
+                    Location = new Point(120, 30 + i * 50),
+                    Width = 200,
+                    Height = 40
+                };
+
+                Controls.Add(nameLabel);
+                Controls.Add(wordName);
+                Controls.Add(definitionLabel);
+                Controls.Add(wordDefinition);
+            }
+
+            Button createCrossword = new Button()
+            {
+                Location = new Point(325, 30),
+                Text = "Створити"
+            };
+            createCrossword.Click += (s, e) =>
+            {
+                AddWords();
+
+                CrossWord crossWord = new CrossWord();
+
+                crossWord.FormClosed += (senser, evArgs) =>
+                {
+                    Close();
+                };
+
+                crossWord.Show();
+                Hide();
+            };
+
+            Controls.Add(createCrossword);
+        }
+
+        private void AddConfig(NumericUpDown x,
+                               NumericUpDown y,
+                               NumericUpDown length,
+                               ComboBox position)
+        {
+            XmlNode config;
+            if (_amount == 0)
+            {
+                _configuration = new XmlDocument();
+                _configuration.Load(_configPath);
+                _configuration.DocumentElement.RemoveAll();
+                config = _configuration.CreateElement("configuration");
+            }
+            else
+            {
+                config = _configuration.DocumentElement.FirstChild;
+            }
+
+            XmlElement wConf = _configuration.CreateElement("wConf");
+
+            XmlAttribute xXml = _configuration.CreateAttribute("x");
+            xXml.AppendChild(_configuration.CreateTextNode(x.Value.ToString()));
+
+            XmlAttribute yXml = _configuration.CreateAttribute("y");
+            yXml.AppendChild(_configuration.CreateTextNode(y.Value.ToString()));
+
+            XmlAttribute lengthXml = _configuration.CreateAttribute("length");
+            lengthXml.AppendChild(_configuration.CreateTextNode(length.Value.ToString()));
+
+            XmlAttribute positionXml = _configuration.CreateAttribute("position");
+            positionXml.AppendChild(_configuration.CreateTextNode(position.SelectedIndex.ToString()));
+
+            wConf.Attributes.Append(xXml);
+            wConf.Attributes.Append(yXml);
+            wConf.Attributes.Append(lengthXml);
+            wConf.Attributes.Append(positionXml);
+
+            config.AppendChild(wConf);
+            _configuration.DocumentElement.AppendChild(config);
+
+
+
+            if (lengths == null)
+                lengths = new List<int>();
+
+            lengths.Add((int)length.Value);
+
+            CreateLabelsAndMoveControls(x, y, length, position);
+
+            _amount++;
+        }
+
+        private void AddWords()
+        {
+            XmlElement words = _configuration.CreateElement("words");
+            _configuration.DocumentElement.AppendChild(words);
+            foreach (Control item in Controls)
+            {
+                if (item is TextBox)
+                {
+                    XmlElement word = _configuration.CreateElement("word");
+
+                    XmlAttribute name = _configuration.CreateAttribute("name");
+                    name.AppendChild(_configuration.CreateTextNode((item as TextBox).Text));
+                    word.Attributes.Append(name);
+
+                    words.AppendChild(word);
+                }
+
+                if (item is RichTextBox)
+                {
+                    XmlNode word = words.LastChild;
+
+                    XmlElement definition = _configuration.CreateElement("definition");
+                    definition.AppendChild(_configuration.CreateTextNode((item as RichTextBox).Text));
+
+                    word.AppendChild(definition);
+                }
+            }
+            _configuration.Save(_configPath);
+        }
+
+        private void CreateLabelsAndMoveControls(params Control[] controls)
+        {
+            foreach (Control item in controls)
+            {
+                Label label = new Label()
+                {
+                    Location = item.Location,
+                    Text = item.Text,
+                    Width = item.Width
+                };
+
+                item.Location = new Point(
+                    item.Location.X,
+                    item.Location.Y + 50
+                    );
+
+                Controls.Add(label);
+            }
+        }
+    }
+}
